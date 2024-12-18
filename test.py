1e742cbf699f888af8adfee2f735c893062e05ee
Santos Bril <Santoslbril@gmail.com>
2024-12-18 15:35:34 +0100

    so now we gotta make it learn

--- test.py
+++ test.py
@@ -4,8 +4,6 @@ import time
 
 env = gym.make('ALE/Pong-v5', render_mode='human')
 
-state, info = env.reset()
-
 num_episodes = 5
 
 for episode in range(num_episodes):
@@ -14,13 +12,13 @@ for episode in range(num_episodes):
 
     state, info = env.reset()
 
-    print(state, info, time.time())
 
     while not done:
+        print(state, info)
         env.render()
 
-         # this is where you would insert your policy
-        action = env.action_space.sample() #RANDOMMMM
+        # this is where you would insert your policy
+        action = env.action_space.sample()
         # print(env.action_space)
 
         next_state, reward, done, truncated, info = env.step(action)
