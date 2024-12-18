a0045ceeb6193cf9de62c3b85262f6a2b7ea9c59
Santos Bril <Santoslbril@gmail.com>
2024-12-18 14:46:59 +0100

    Things work now?

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import gymnasium as gym
+import ale_py
+import time
 
 env = gym.make('ALE/Pong-v5', render_mode='human')
 
@@ -12,12 +14,14 @@ for episode in range(num_episodes):
 
     state, info = env.reset()
 
+    print(state, info, time.time())
+
     while not done:
         env.render()
 
          # this is where you would insert your policy
-        action = env.action_space.sample()
         action = env.action_space.sample() #RANDOMMMM
+        # print(env.action_space)
 
         next_state, reward, done, truncated, info = env.step(action)
 
