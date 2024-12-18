ad915bf89c3a20a525d3b18172e5c624fac8db68
Jia Hao Zhang <j.h.zhang@student.rug.nl>
2024-12-18 13:43:17 +0100

    updated pong

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 import threading
-from math import (cos, sin, pi)
+from math import (cos, sin, pi, atan2)
 from time import sleep
 
 
@@ -9,7 +9,7 @@ render_queue: list = []
 running: bool = True
 
 
-def render(screen: pygame.Surface, clock: pygame.time.Clock):
+def render(screen: pygame.Surface, clock: pygame.time.Clock, frame_rate: float):
     while running:
         screen.fill("black")
 
@@ -18,7 +18,7 @@ def render(screen: pygame.Surface, clock: pygame.time.Clock):
 
         pygame.display.flip()
 
-        clock.tick(999)
+        clock.tick(frame_rate)
 
 
 def main():
@@ -27,20 +27,23 @@ def main():
     # overall game settings
     pygame.init()
     screen_width, screen_height = (1280, 720)
+    frame_rate: float = 999
+    simulation_rate: float = 165
+
     screen = pygame.display.set_mode((screen_width, screen_height))
     clock = pygame.time.Clock()
 
     # player paddle set up
-    player_paddle = pygame.Rect(0, 0, 25, 100)
-    player_speed = 50
+    player_paddle = pygame.Rect(0, 0, 25, 100).move(pygame.Vector2(0, screen.get_height() / 2))
+    player_speed: float = 3000 / simulation_rate
 
     # ball set up
     ball = pygame.Rect(0, 0, 25, 25).move(pygame.Vector2(screen.get_width() / 2,
                                                          screen.get_height() / 2))
-    ball_velocity: int = 5
+    ball_velocity: float = 500 / simulation_rate
     ball_angle: float = random.uniform(0.25 * pi, 0.75 * pi)
 
-    render_thread = threading.Thread(target=render, args=(screen, clock,))
+    render_thread = threading.Thread(target=render, args=(screen, clock, frame_rate,))
     render_thread.start()
     while running:
         for event in pygame.event.get():
@@ -51,9 +54,11 @@ def main():
 
         # region player logic
         pre_render_queue.append(player_paddle)
-        move_player: tuple[int, int] = (0, 0)
-        paddle_top, paddle_bottom, paddle_right = (player_paddle.top, player_paddle.bottom,
-                                                   player_paddle.right)
+        move_player: tuple[float, float] = (0, 0)
+        paddle_top, paddle_bottom, paddle_right, paddle_center_y = (player_paddle.top,
+                                                                    player_paddle.bottom,
+                                                                    player_paddle.right,
+                                                                    player_paddle.centery)
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
@@ -77,12 +82,15 @@ def main():
                                         ball_velocity * sin(ball_angle))
 
         ball_top, ball_bottom, ball_left, ball_right = ball.top, ball.bottom, ball.left, ball.right
+        ball_x, ball_y = ball.center
 
         if (ball_top + ball_y_offset >= paddle_top and
             ball_bottom + ball_y_offset <= paddle_bottom and
                 ball_left + ball_x_offset <= paddle_right):
             ball_offset = [paddle_right - ball_left, ball_y_offset]
-            ball_angle = 2 * pi - ball_angle
+            ball_angle = max(min(atan2((ball_y - paddle_center_y), (ball_x - paddle_right)),
+                                 0.75 * pi),
+                             0.25 * pi)
         else:
             if ball_right + ball_x_offset <= screen_width and ball_left + ball_x_offset >= 0:
                 ball_offset.append(ball_x_offset)
@@ -110,7 +118,7 @@ def main():
 
         render_queue = pre_render_queue
 
-        sleep(1/60)
+        sleep(1/simulation_rate)
 
 
 if __name__ == '__main__':
