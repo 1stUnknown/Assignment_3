f1e4127e418d43c5de8a263766750f2e2c74403d
Jia Hao Zhang <j.h.zhang@student.rug.nl>
2024-12-19 13:48:31 +0100

    it predicts something

--- homebrew/nn.py
+++ homebrew/nn.py
@@ -1,10 +1,10 @@
 from torch import nn
+import torch
 
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(2 + 2 + 2, 512),
             nn.ReLU(),
@@ -14,10 +14,14 @@ class NeuralNetwork(nn.Module):
         )
 
     def forward(self, x):
-        x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
 
 
 model = NeuralNetwork().to("cpu")
 print(model)
+X = torch.rand(1, 6, device="cpu")
+logits = model(X)
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
