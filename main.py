ebdafca4caa5b6fa4d410f769075f0369d463650
Santos Bril <Santoslbril@gmail.com>
2024-12-21 13:56:18 +0100

    worked for 2.5 hours

--- main.py
+++ main.py
@@ -1,32 +1,70 @@
 import gymnasium as gym
 import ale_py
+import threading
 from gymnasium.wrappers import GrayscaleObservation
-# from homebrew.nn import NeuralNetwork
+from neural_network import NeuralNetwork
 
-env = gym.make('ALE/Pong-v5', render_mode='human')
-env = GrayscaleObservation(env)
+nn_results = []
 
-num_episodes = 5
+def play_pong(nn: NeuralNetwork) -> None:
+    global nn_results
+    env = gym.make('ALE/Pong-v5')
+    env = GrayscaleObservation(env)
+    num_episodes = 5
 
-for episode in range(num_episodes):
-    done = False
-    total_reward: float = 0
+    whole_match_reward = 0
+    for episode in range(num_episodes):
+        done = False
+        total_reward: float = 0
 
-    state, info = env.reset()
+        state, info = env.reset()
 
-    while not done:
-        env.render()
+        while not done:
 
-        # this is where you would insert your policy
-        action = env.action_space.sample()
+            # give current states to the NNs
+            
+            # nn_action = nn.response(state)
 
-        next_state, reward, done, truncated, info = env.step(action)
+            # this is where you would insert your policy
 
-        total_reward += float(reward)
+            action = nn.predict_action(state.reshape((1, 1, 210, 160)))
+            
+            next_state, reward, done, truncated, info = env.step(action)
 
-        state = next_state
+            total_reward += float(reward)
 
-    print(f"Episode {episode + 1} finished with total reward: {total_reward}")
+            state = next_state
 
-# Close the environment
-env.close()
+
+        print(f"Episode {episode + 1} finished with total reward: {total_reward}")
+        whole_match_reward += total_reward
+
+    # Close the
+    env.close()
+    
+    nn_results.append((whole_match_reward))
+
+
+def main():
+    amount_of_nns = 5
+
+    list_of_nn: list[NeuralNetwork] = [NeuralNetwork() for _ in range(amount_of_nns)]
+    
+    threads = []
+
+    for nn in list_of_nn:
+        threads.append(threading.Thread(target=play_pong, args=(nn,)))
+    
+    # Start all threads.
+    for t in threads:
+        t.start()
+
+    # Wait for all threads to finish.
+    for t in threads:
+        t.join()
+
+    print(nn_results)
+
+
+if __name__ == "__main__":
+    main()
