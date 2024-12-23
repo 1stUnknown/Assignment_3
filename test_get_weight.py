cf9f2f15841e3974c22b1fa72a6bca2a782ad884
Jia Hao Zhang <j.h.zhang@student.rug.nl>
2024-12-23 22:51:41 +0100

    added method to get the weights of a model

--- test_get_weight.py
+++ test_get_weight.py
@@ -1,14 +1,11 @@
 from neural_network import NeuralNetwork
-import torch
 
 
 def main():
     model = NeuralNetwork()
     print(model)
 
-    for layer in model.linear_relu_stack:
-        if not isinstance(layer, torch.nn.ReLU):
-            print(layer.weight)
+    print(model.weights())
 
 
 if __name__ == '__main__':
