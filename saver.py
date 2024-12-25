94d73801069405b4c467e2141372eb222a6a7416
Jia Hao Zhang <j.h.zhang@student.rug.nl>
2024-12-25 13:20:50 +0100

    indentation changes to saver file and made an if statement into elif since it is a error checker

--- saver.py
+++ saver.py
@@ -13,24 +13,26 @@ def saving(weights: list[np.ndarray], file_name: str) -> None:
 
     with open(f"./savedweights/{file_name}.json", "w") as file:
         json.dump({index: weight.tolist() for index, weight in
-                    enumerate(weights)},
-                    file, indent=4)
+                   enumerate(weights)},
+                  file, indent=4)
 
 
 def loading(file_name: str) -> list[np.ndarray]:
     """loads the weights from the savedweigths directory"""
     if not os.path.exists("./savedweights/"):
         raise FileNotFoundError("savedweights directory not found")
-    if not os.path.exists(f"./savedweights/{file_name}.json"):
+    elif not os.path.exists(f"./savedweights/{file_name}.json"):
         raise FileNotFoundError(f"{file_name} file not found")
 
     try:
         with open(f"./savedweights/{file_name}.json", "r") as file:
             dict_of_file = json.load(file)
+
         return_list = []
         for key in dict_of_file.keys():
             return_list.append(np.array(dict_of_file[key]))
+
         return return_list
     except ValueError:
         raise ValueError("couldn't import from quicksave,"
-                            + " the file might be corrupted")
+                         + " the file might be corrupted")
