a7adb848bcb28aee6e4a9210c1cb96841afede9e
Santos Bril <Santoslbril@gmail.com>
2025-01-08 12:11:29 +0100

    worked for an hour
    added testing and validation and main loop

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import gymnasium as gym
-from gymnasium.wrappers import GrayscaleObservation
-import ale_py
-
 import numpy as np
 
+import ale_py
+
+from gymnasium.wrappers import GrayscaleObservation
+from random import choice, randint
 from os import listdir
 from os.path import isdir
-
 from threading import Thread
-
-from random import choice
 
 from neural_network import NeuralNetwork
 from saver import saving, loading
@@ -18,7 +16,7 @@ from utility import get_top
 nn_results: list[tuple[int, float, np.ndarray]] = []
 
 
-def play_pong(nn: NeuralNetwork, id: int, num_episodes: int = 5) -> None:
+def play_pong(nn: NeuralNetwork, id: int, num_episodes: int = 5, test: tuple = (0,100)) -> list[float]:
     global nn_results
     env = gym.make('ALE/Pong-v5')
     env = GrayscaleObservation(env)
@@ -28,7 +26,7 @@ def play_pong(nn: NeuralNetwork, id: int, num_episodes: int = 5) -> None:
         done = False
         total_reward: float = 0
 
-        state, info = env.reset()
+        state, info = env.reset(randint(test[0], test[1]))
 
         while not done:
 
@@ -74,45 +72,127 @@ def main():
 
         for index in range(len(saved_weights), amount_of_nns):
             list_of_nn[index].set_weights(choice(saved_weights), True)
-
     try:
-        while True:
-            threads = []
-
-            for index, nn in enumerate(list_of_nn):
-                threads.append(Thread(target=play_pong,
-                                      args=(nn, index, num_episodes,)))
-
-            # Start all threads.
-            for t in threads:
-                t.start()
-
-            # Wait for all threads to finish.
-            for t in threads:
-                t.join()
-
-            # order the results
-            top_weights, ids_of_top_results = get_top(nn_results, top_x)
-
-            for index in range(amount_of_nns):
-                if index in ids_of_top_results:
-                    continue
-
-                list_of_nn[index].set_weights(choice(top_weights), True)
-
-            nn_results.clear()
+        main_loop(list_of_nn, top_x=top_x, num_episodes=num_episodes)
     except KeyboardInterrupt:
-        # save everything the top x
+        # TODO save the total rewards between test and validate
         print("[INFO] Exiting after KeyboardInterupt; proceeding to save weights before exiting")
-
-        for t in threads:
-            t.join()
-
         top_weights, _ = get_top(nn_results, top_x)
         for index, weight in enumerate(top_weights):
             saving(weight, f"{index}")
         print("[INFO] Exiting after Saving!")
 
 
+def testing() -> float:
+    amount_of_nns = 10
+    top_x = 3
+    num_episodes = 10
+
+    list_of_nn: list[NeuralNetwork] = [NeuralNetwork() for _ in
+                                       range(amount_of_nns)]
+
+    if isdir("./savedweights"):
+        file_names: list[str] = listdir("./savedweights")
+
+        saved_weights: list[list[np.ndarray]] = []
+        for name in file_names:
+            data = loading(name)
+            saved_weights.append(data)
+
+        for index, weight in enumerate(saved_weights):
+            list_of_nn[index].set_weights(weight)
+
+        for index in range(len(saved_weights), amount_of_nns):
+            list_of_nn[index].set_weights(choice(saved_weights), True)
+
+    results = main_loop(list_of_nn, length_of_running_program=100, top_x=top_x,
+              num_episodes=num_episodes, modify_weights=False)
+    
+    return sum(results)/len(results)
+    
+
+def validate() -> float:
+    """
+    once done training, validate the neural networks
+    """
+    amount_of_nns = 10
+    top_x = 3
+    num_episodes = 10
+
+    list_of_nn: list[NeuralNetwork] = [NeuralNetwork() for _ in
+                                       range(amount_of_nns)]
+
+    if isdir("./savedweights"):
+        file_names: list[str] = listdir("./savedweights")
+
+        saved_weights: list[list[np.ndarray]] = []
+        for name in file_names:
+            data = loading(name)
+            saved_weights.append(data)
+
+        for index, weight in enumerate(saved_weights):
+            list_of_nn[index].set_weights(weight)
+
+        for index in range(len(saved_weights), amount_of_nns):
+            list_of_nn[index].set_weights(choice(saved_weights), True)
+
+    results = main_loop(list_of_nn, range_of_seed=(100, 200),
+              length_of_running_program=100, top_x = top_x,
+              num_episodes = num_episodes, modify_weights=False)
+    
+    return sum(results)/len(results)
+    
+
+def main_loop(list_of_nn: list[NeuralNetwork], 
+              range_of_seed: tuple = (0,100), 
+              length_of_running_program: int = float("inf"),
+              top_x: int = 3, num_episodes: int = 10, 
+              modify_weights: bool = True) -> list[float]:
+
+    if top_x >= len(list_of_nn):
+        raise ValueError("top x is equal or greater than"
+                         + "the amount of neural networks given")
+
+
+    i = 0
+    list_of_match_results = []
+    while i < length_of_running_program:
+        threads = []
+
+        for index, nn in enumerate(list_of_nn):
+            threads.append(Thread(target=play_pong,
+                                    args=(nn, 
+                                          index, 
+                                          num_episodes, 
+                                          range_of_seed,)))
+
+        # Start all threads.
+        for t in threads:
+            t.start()
+
+        # Wait for all threads to finish.
+        for t in threads:
+            t.join()
+
+        if modify_weights:
+            # order the results
+            top_weights, ids_of_top_results = get_top(nn_results, top_x)
+
+            for index in range(len(list_of_nn)):
+                if index in ids_of_top_results:
+                    continue
+
+                list_of_nn[index].set_weights(choice(top_weights), True)
+        list_of_match_results.append(item[1] for item in nn_results)
+        # I don't know if this would work. I would just use list comprehension instead of slicing
+        nn_results.clear()
+        i += 1
+
+    return list_of_match_results
+
+
 if __name__ == "__main__":
     main()
+    #testing_results = testing()
+    #validation_results = validate()
+    #print(validation_results - testing_results)
